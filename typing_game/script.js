e576bbcaeec352e1a53d10a14959999ba4a0701c
yassinenmila <nmilayassine0000@gmail.com>
2025-10-30 18:54:44 +0100

    update about file javascript

--- typing_game/script.js
+++ typing_game/script.js
@@ -1,13 +1,19 @@
 let text = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry when an unknown printer took a galley of type and scrambled it to make a type specimen book';
 text = melange(text);
 span(text);
-const input = document.querySelector("#typing");
-
+const spans=document.querySelectorAll("#words span");
+let position=0;
 
 document.addEventListener("keydown", function (event) {
-    touch = event.key;
-    let touch;
-    
+    const touch = event.key;
+    if(touch.length!==1)return;
+    if(touch===text[position]){
+        spans[position].style.color="green";
+    }
+    else{
+        spans[position].style.color="tomato";
+    }
+    position++;
 });
 
 function span (text){
